e3703ba87504f247b8bcba27a1be2c19f1440549
Swich <andruha313@gmail.com>
2022-07-05 14:19:12 +0300

    simpliest counter with REDUX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+
+const initialState = 0;
+
+const reducer = (state = 0, action) => {   //reducer должен быть чистой функцией
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        case 'DEC':
+            return state -1;
+        case 'RND':
+            return state * action.payload;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+const inc = () => {
+    return {
+        type: 'INC'
+    }
+}                                           //ф-ция Action Creator
+
+const dec = () => ({type: 'DEC'});          //ф-ция Action Creator
+//const rnd = (value) => ({type: 'RND', payload: value});
+
+document.getElementById('inc').addEventListener('click', ()=> {
+    store.dispatch(inc());
+})
+
+document.getElementById('dec').addEventListener('click', ()=> {
+    store.dispatch(dec());
+})
+
+document.getElementById('rnd').addEventListener('click', ()=> {
+    const value  = Math.floor(Math.random() * 10);
+    store.dispatch({type: 'RND', payload: value});
+})
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <>
+
+   </>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
