6a31ccaf9809ecac74e34d0ebc210b8fbfc04d79
Swich <andruha313@gmail.com>
2022-07-07 15:16:44 +0300

    connect React with React-Redux using hooks useSelector&useDispatch from REACT-REDUX

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,19 @@
-import {connect} from "react-redux";
-import * as actions from "../actions";
-import {bindActionCreators} from "redux";
+import {useSelector, useDispatch} from "react-redux";
+import {inc, dec, rnd} from "../actions";
 
-const Counter = ({counter, inc, dec, rnd}) => {
+const Counter = () => {
+    const counter = useSelector(state => state.counter)
+    const dispatch = useDispatch();
+
     return (
         <div className="jumbotron">
             <h1>{counter}</h1>
-            <button onClick={dec} className="btn btn-primary">DEC</button>
-            <button onClick={inc} className="btn btn-primary">INC</button>
-            <button onClick={rnd} className="btn btn-primary">RND</button>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
         </div>
     )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
 
-const mapDispatchtoProps = (dispatch) => {
-    return bindActionCreators(actions, dispatch)                            //более короткая запись чем закоментированный фрагмент
-    // const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-    // return {
-    //         inc,
-    //         dec,
-    //         rnd
-    //     }
-    }
-
-// export default connect(mapStateToProps, mapDispatchtoProps)(Counter);
-export default connect(mapStateToProps, actions)(Counter);                  //можно не прописывать ф-цию mapDispatchtoProps вручную
-                                                                            // а передать в connect actions и ф-ция connect сама обернет
-                                                                            //actions в dispatch
+export default Counter;
